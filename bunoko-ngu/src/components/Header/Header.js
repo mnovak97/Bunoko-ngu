d556bc597d9c736077ee4f598ebaf459b7eda0a5
lobomanz <91606969+lobomanz@users.noreply.github.com>
2023-05-03 04:41:55 +0200

    add localization

--- bunoko-ngu/src/components/Header/Header.js
+++ bunoko-ngu/src/components/Header/Header.js
@@ -1,10 +1,30 @@
-import React    from "react";
-import template from "./Header.jsx";
+import "./Header.css";
+import React, { useState } from 'react';
+import {localization} from "../../localization/localization"; 
+function Header() {
+  const [open, setOpen] = useState(false);
 
-class Header extends React.Component {
-  render() {
-    return template.call(this);
-  }
-}
+  const handleOpen = () => {
+    setOpen(!open);
+  };
+
+  return (
+    <header>
+      <nav>
+        <h1>bunoko n.g.u.</h1>
+        <ul>
+          <li>{open ? <p>{localization.croatian.header.home}</p> : <p>{localization.english.header.home}</p>}</li>
+          {/* <li>{open ? {localization.croatian.header.about} : {localization.english.header.about}}</li> */}
+          <li>open ? {localization.croatian.header.services} : {localization.english.header.services}</li>
+          <li>open ? {localization.croatian.header.contact} : {localization.english.header.contact}</li>
+        </ul>
+        <div  onClick={handleOpen} className="dropdown">
+        {open ? <div>HR</div> : <div className="dropdownContainer">HR<div className="dropdownContent"><p>HR</p><p>ENG</p></div></div>}
+        </div>
+      </nav>
+    </header>
+  );
+};
 
 export default Header;
+

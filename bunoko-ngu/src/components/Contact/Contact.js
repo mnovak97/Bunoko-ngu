efff87e17f05307999468d93ac293e8728f74907
mnovak97 <63095399+mnovak97@users.noreply.github.com>
2023-05-16 13:40:25 +0200

    Add routing and contact page

---
+++ bunoko-ngu/src/components/Contact/Contact.js
@@ -0,0 +1,13 @@
+import './Contact.scss'
+import { useTranslation } from "react-i18next";
+import React, { useState } from 'react';
+
+function Contact() {
+    return (
+        <div className='mainDiv'>
+            <img src='https://www.si.com/.image/ar_1:1%2Cc_fill%2Ccs_srgb%2Cfl_progressive%2Cq_auto:good%2Cw_1200/MTk0MDk2NDQxMzY5NTY4Nzcz/charles-leclerc.jpg' alt=''></img>
+        </div>
+    );
+};
+
+export default Contact;
